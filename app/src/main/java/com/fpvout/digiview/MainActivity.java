254af3f257976c551ace31ec3fffd7752374a75d
Olivier Mouren <mouren.olivier@gmail.com>
2021-05-14 10:25:26 +0200

    A REAL Fullscreen <3

--- app/src/main/java/com/fpvout/digiview/MainActivity.java
+++ app/src/main/java/com/fpvout/digiview/MainActivity.java
@@ -37,8 +37,12 @@ public class MainActivity extends AppCompatActivity implements UsbDeviceListener
 
         // Hide top bar and status bar
         View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 

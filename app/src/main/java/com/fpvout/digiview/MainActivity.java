2297611e983cb84182700fc39d09c30777827097
Jimmy Lucidarme <jlucidar@gmail.com>
2021-05-21 19:27:39 +0200

    fix fullscreen recovery on Activity resume

--- app/src/main/java/com/fpvout/digiview/MainActivity.java
+++ app/src/main/java/com/fpvout/digiview/MainActivity.java
@@ -258,6 +258,18 @@ public class MainActivity extends AppCompatActivity implements UsbDeviceListener
         super.onResume();
         Log.d(TAG, "APP - On Resume");
 
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
         if (!usbConnected) {
             if (searchDevice()) {
                 Log.d(TAG, "APP - On Resume usbDevice device found");
